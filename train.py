137a47b026bc48b4ec0097d6fe4f087820f25937
reoneo97 <reoneo97@gmail.com>
2021-05-21 10:14:50 +0800

    added training using config.yaml file

--- train.py
+++ train.py
@@ -1,14 +1,23 @@
 
 from pytorch_lightning import Trainer
-
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 import os
 from models import VAE
 
+import argparse
+import yaml
+
+def load_config(path="config.yaml"):
+    config = yaml.load(open(path),yaml.SafeLoader)
+    return config["training_params"],config["model_params"]
+
 
 if __name__ == "__main__":
-    model =  VAE(alpha = 50,dataset="fashion-mnist")
+    training_params,model_params = load_config()
+
+    model =  VAE(**model_params)
     print("==== Model Architecture ====")
-    print(VAE)
-    trainer = Trainer(gpus = 1,auto_lr_find=True,max_epochs=25)
-    trainer.fit(VAE)
+    print(model)
+    trainer = Trainer(**training_params)
+    trainer.fit(model)
+    
